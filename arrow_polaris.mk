1993be33fe69d6be26109fdfd80454449caf5c27
palaych <palaychm@yandex.ru>
2019-01-07 08:05:53 +0000

    polaris: Add ArrowOS

--- arrow_polaris.mk
+++ arrow_polaris.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_polaris
+PRODUCT_NAME := arrow_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
